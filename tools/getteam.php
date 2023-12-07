e76eb454914f5c4967682b58ece06d24f24efc61
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2023-12-07 16:11:17 -0600

    Get Team
    Check if the GET request is appropriate. Sanitize the input, then get the team from the DB for the given ID. If team does not exist, then it will redirect them to the Home Page.

---
+++ tools/getteam.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!$_GET || !isset($_GET["id"])) {
+    header("Location: index.php");
+    exit();
+}
+
+require("admin/connect.php");
+
+$team_id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
+
+$query = "SELECT * FROM teams WHERE team_id = :team_id";
+
+$statement = $db->prepare($query);
+$statement->bindValue(":team_id", $team_id, PDO::PARAM_INT);
+$statement->execute();
+
+$team = $statement->fetch();
+
+if (!$team) {
+    header("Location: index.php");
+    exit();
+}
+
+?>
