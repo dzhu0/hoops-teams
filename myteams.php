c2dc1ffb61ed6be04f1d855e4c788b8e39b006ab
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2023-12-14 15:45:56 -0600

    My Team Page Update
    Add new column to each team that show the team's league name. No longer need to click on sort button to sort the teams.

--- myteams.php
+++ myteams.php
@@ -2,8 +2,10 @@
 
 session_start();
 
-require("admin/authenticate.php");
-require("admin/connect.php");
+require("tools/authenticate.php");
+require("tools/connect.php");
+require("tools/getthumbnail.php");
+require("tools/getleaguename.php");
 
 $sort = isset($_GET["sort"]) ? $_GET["sort"] : "team_name";
 
@@ -20,10 +22,7 @@ $statement = $db->prepare($query);
 $statement->bindValue(":user_id", $_SESSION["user_id"], PDO::PARAM_INT);
 $statement->execute();
 
-function getThumbnail($logo): string
-{
-    return pathinfo($logo, PATHINFO_DIRNAME) . DIRECTORY_SEPARATOR . pathinfo($logo, PATHINFO_FILENAME) . "_thumbnail." . pathinfo($logo, PATHINFO_EXTENSION);
-}
+$teams = $statement->fetchAll();
 
 ?>
 
@@ -42,31 +41,27 @@ function getThumbnail($logo): string
         <main>
             <h2>My Teams</h2>
 
-            <form class="form-sort">
+            <form class="form-horizontal form-sort">
                 <label for="sort">Sort by:</label>
-                <select name="sort" id="sort">
+                <select name="sort" id="sort" onchange="this.form.submit()">
                     <option value="team_name" <?php if ($sort === "team_name" || !isset($_GET["sort"])): ?>selected<?php endif ?>>Team Name</option>
                     <option value="arena" <?php if ($sort === "arena"): ?>selected<?php endif ?>>Arena</option>
-                    <option value="home_city" <?php if ($sort === "home_city"): ?>selected<?php endif ?>>Home City
-                    </option>
-                    <option value="head_coach" <?php if ($sort === "head_coach"): ?>selected<?php endif ?>>Head Coach
-                    </option>
+                    <option value="home_city" <?php if ($sort === "home_city"): ?>selected<?php endif ?>>Home City</option>
+                    <option value="head_coach" <?php if ($sort === "head_coach"): ?>selected<?php endif ?>>Head Coach</option>
                 </select>
-
-                <button>Sort</button>
             </form>
 
-            <?php if ($statement->rowCount()): ?>
+            <?php if (count($teams)): ?>
                 <div class="teams">
-                    <?php while ($team = $statement->fetch()): ?>
+                    <?php foreach ($teams as $team): ?>
                         <details open>
                             <summary>
                                 <?php if ($team["logo"]): ?>
                                     <img src="<?= getThumbnail($team["logo"]) ?>" alt="<?= $team["team_name"] ?>" class="logo">
                                 <?php else: ?>
-                                    <div class="no-logo small">
+                                    <span class="no-logo small">
                                         No Logo
-                                    </div>
+                                    </span>
                                 <?php endif ?>
                                 <h2>
                                     <a href="team.php?id=<?= $team["team_id"] ?>">
@@ -83,11 +78,14 @@ function getThumbnail($logo): string
                             <p><b>Head Coach:</b>
                                 <?= $team["head_coach"] ?>
                             </p>
+                            <p><b>League Name:</b>
+                                <?= getLeagueName($db, $team["league_id"]) ?>
+                            </p>
                         </details>
-                    <?php endwhile ?>
+                    <?php endforeach ?>
                 </div>
             <?php else: ?>
-                <h2>You have no team.</h2>
+                <p class="center">You have no team.</p>
             <?php endif ?>
         </main>
     </div>
