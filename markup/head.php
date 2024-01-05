102f71ccf38ad748d5dda5a1f56576e87ccd4d71
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2024-01-05 07:53:43 -0600

    Update head.php
    Add description and author meta tags.

--- markup/head.php
+++ markup/head.php
@@ -1,4 +1,6 @@
 <meta charset="UTF-8">
+<meta name="description" content="HoopsTeams is a Content Management System (CMS) for basketball teams. The system includes features for managing teams, leagues, comments, and user authentication.">
+<meta name="author" content="Daniel Zhu">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="css/mvp.css">
